189bc9db57f4ccd4ab4e4bcb233e90d2289ab184
Abdulal-noman <mdroman45678910@gamil.com>
2025-08-16 17:30:41 +0600

    add dark mode in upcommingHoem

--- src/Pages/Home/UpcommingHome.jsx
+++ src/Pages/Home/UpcommingHome.jsx
@@ -5,7 +5,6 @@ import Loader from "../Loader/Loader";
 import SingleUpcommingMeals from "../Dashbord/Admin/UpcomingMeals/SingleUpcommingMeals";
 import Dot from "./Dot";
 
-
 const UpcommingHome = () => {
   const axiosSecure = useAxiosSecure();
 
@@ -13,7 +12,6 @@ const UpcommingHome = () => {
     queryKey: ["upcomingMeals"],
     queryFn: async () => {
       const res = await axiosSecure.get("/upcoming-meals");
-      console.log(res.data)
       return res.data;
     },
   });
@@ -21,29 +19,35 @@ const UpcommingHome = () => {
   if (isLoading) return <Loader />;
   if (isError)
     return (
-      <p className="text-center mt-10 text-red-600">
+      <p className="text-center mt-10 text-red-600 dark:text-red-400">
         Failed to load upcoming meals.
       </p>
     );
 
   return (
     <>
-
       <Dot />
-      <div className="bg-orange-50">
+      <div className="bg-orange-50 dark:bg-slate-900">
         <div className="flex justify-center">
-            <p className="inline-block bg-orange-100 dark:bg-orange-700 text-orange-600 dark:text-orange-100 px-4 py-2 rounded-full text-xs sm:text-sm md:text-base lg:text-lg font-medium my-6 flxe items-center">UpcommingMeals</p>
+          <p className="inline-block bg-orange-100 dark:bg-orange-700 text-orange-600 dark:text-orange-100 px-4 py-2 rounded-full text-xs sm:text-sm md:text-base lg:text-lg font-medium my-6 ">
+            Upcoming Meals
+          </p>
         </div>
         <div className="container mx-auto px-4 py-10">
           {/* Meals Grid */}
           <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {data?.map((product) => (
-              <SingleUpcommingMeals key={product._id} product={product} />
-            ))}
+            {data?.length ? (
+              data.map((product) => (
+                <SingleUpcommingMeals key={product._id} product={product} />
+              ))
+            ) : (
+              <p className="col-span-full text-center text-gray-500 dark:text-gray-400">
+                No upcoming meals available.
+              </p>
+            )}
           </div>
         </div>
       </div>
-
     </>
   );
 };
